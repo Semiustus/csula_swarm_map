cd1708f8586776ff758c79ddc96b9f0b5f7d62ab
Brian <wartime@outlook.com>
2016-03-19 22:16:52 -0700

    formating layers

--- src/gridmap/grid_map_demos/src/grid_test.cpp
+++ src/gridmap/grid_map_demos/src/grid_test.cpp
@@ -1,66 +1,69 @@
 #include <ros/ros.h>
 #include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_msgs/GridMap.h>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <limits>
 
 using namespace grid_map;
-using namespace ros;
 
 int main(int argc, char** argv)
 {
   // Initialize node and publisher.
-  init(argc, argv, "grid_test");
-  NodeHandle nh("~");
-  Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+  ros::init(argc, argv, "grid_test");
+  ros::NodeHandle nh("~");
+  ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
 
   // Create grid map.
-  GridMap map({"layer"});
+  GridMap map({"density"});
   map.setFrameId("map");
-  map.setGeometry(Length(0.7, 0.7), 0.01, Position(0.0, 0.0));
-  ROS_INFO("Created map with size %f x %f m (%i x %i cells).\n The center of the map is located at (%f, %f) in the %s frame.",
-    map.getLength().x(), map.getLength().y(),
-    map.getSize()(0), map.getSize()(1),
-    map.getPosition().x(), map.getPosition().y(), map.getFrameId().c_str());
-  map["layer"].setRandom();
+  map.setGeometry(Length(10.0, 10.0), 0.09);
+  ROS_INFO("Created map with size %f x %f m (%i x %i cells).",
+  map.getLength().x(), map.getLength().y(),
+  map.getSize()(0), map.getSize()(1));
 
-  bool useMoveMethod = true;
+  //loops map
+  ros::Rate rate(30.0);
+
+  bool getlow;
+
+
   while (nh.ok()) {
+    ros::Time time = ros::Time::now();
 
-    if (useMoveMethod) {
-      ROS_INFO("Using the `move(...)` method.");
+    // testing updates
+
+    if ( abs(time.toSec()) % 2 == 0){
+      getlow = true;
     } else {
-      ROS_INFO("Using the `setPosition(...)` method.");
+      getlow = false;
     }
 
-    // Work with temporary map in a loop.
-    GridMap tempMap(map);
-    Rate rate(10.0);
-    ros::Time startTime = ros::Time::now();
-    ros::Duration duration(0.0);
+    // testing layer
 
-    while (duration <= ros::Duration(10.0)) {
-      ros::Time time = ros::Time::now();
-      duration = time - startTime;
+    if (getlow){
+      map["density"].setConstant(.9);
+    } else {
 
-      // Change position of the map with either the `move` or `setPosition` method.
-      const double t = duration.toSec();
-      Position newPosition = 0.03 * t * Position(cos(t), sin(t));
 
-      if (useMoveMethod) {
-        tempMap.move(newPosition);
-      } else {
-        tempMap.setPosition(newPosition);
+      // Add elevation and surface normal (iterating through grid map and adding data).
+      for (GridMapIterator it(map); !it.isPastEnd(); ++it) {
+        Position position;
+        map.getPosition(*it, position);
+        map.at("density", *it) = -0.04 + 0.2 * std::sin(3.0 * time.toSec() + 5.0 * position.y()) * position.x();
       }
-
-      // Publish grid map.
-      tempMap.setTimestamp(time.toNSec());
-      grid_map_msgs::GridMap message;
-      GridMapRosConverter::toMessage(tempMap, message);
-      publisher.publish(message);
-      ROS_DEBUG("Grid map (duration %f) published with new position [%f, %f].",
-                duration.toSec(), tempMap.getPosition().x(), tempMap.getPosition().y());
-      rate.sleep();
     }
 
-    useMoveMethod = !useMoveMethod;
+    // Publish grid map.
+    map.setTimestamp(time.toNSec());
+    grid_map_msgs::GridMap message;
+    GridMapRosConverter::toMessage(map, message);
+    publisher.publish(message);
+    ROS_INFO_THROTTLE(1.0, "Grid map (timestamp %f) published.", message.info.header.stamp.toSec());
+
+    rate.sleep();
+
   }
 
   return 0;
